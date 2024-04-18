7046304a5b39edbde94a549ad42431e031095770
nicky.schobben@student.pxl.be <nicky.schobben@student.pxl.be>
2024-04-18 16:24:38 +0200

    test2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8baf5jv0fqg"
+    "revision": "0.ggr8ka1s628"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
